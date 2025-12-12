adaf9bad191e62fe8ba5ce848a77fe2ac22cce54
Wizza <moatazbesher2011@gmail.com>
2025-12-12 23:34:01 +0200

    Creating fraction structure

---
+++ index.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+int Num;
+int Denom;
+} FRAC;
+int get_gcd(int x,int y)
+{
+    if(x%y==0)
+        return y;
+    return get_gcd(y,x%y);
+}
+FRAC Multiply_fractions (FRAC n , FRAC m)
+{
+FRAC ans;
+ans.Num= n.Num * m.Num;
+ans.Denom = n.Denom * m.Denom;
+int r=get_gcd(ans.Num,ans.Denom);
+ans.Num/=r;
+ans.Denom/=r;
+return ans;
+}
+FRAC Divide_fractions (FRAC n , FRAC m)
+{
+    FRAC ans;
+    ans.Num=n.Num*m.Denom;
+    ans.Denom=n.Denom*m.Num;
+    int r=get_gcd(ans.Num,ans.Denom);
+    ans.Num/=r;
+    ans.Denom/=r;
+    return ans;
+}
+FRAC ADD_fractions (FRAC n , FRAC m)
+{
+    FRAC ans;
+    ans.Num=n.Num*m.Denom+m.Num*n.Denom;
+    ans.Denom=n.Denom*m.Denom;
+    int r=get_gcd(ans.Num,ans.Denom);
+    ans.Num/=r;
+    ans.Denom/=r;
+    return ans;
+}
+int main()
+{
+FRAC N1, N2,N;
+printf("Enter the numerator of the first fraction:");
+scanf("%d",&N1.Num);
+printf("Enter the denomenator of the first fraction:");
+scanf("%d",&N1.Denom);
+printf("Enter the numerator of the second fraction:");
+scanf("%d",&N2.Num);
+printf("Enter the denomenator of the second fraction:");
+scanf("%d",&N2.Denom);
+N=Multiply_fractions(N1,N2);
+printf("result of N1*N2= %d / %d\n", N.Num, N.Denom);
+N=Divide_fractions(N1,N2);
+printf("result of N1/N2= %d / %d\n", N.Num, N.Denom);
+N=ADD_fractions(N1,N2);
+printf("result of N1+N2= %d / %d\n", N.Num, N.Denom);
+return 0;
+}
+
